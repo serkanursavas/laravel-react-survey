7c649a3dc8921f045035da9706cc99c1190e903c
serkanursavas <serkan.ursavas@icloud.com>
2023-05-01 23:46:33 +0300

    creating default layout

---
+++ react/src/components/GuestLayout.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import { LockClosedIcon } from "@heroicons/react/outline";
+
+export default function GuestLayout() {
+  return (
+    <div>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+        </div>
+
+        <Outlet />
+      </div>
+    </div>
+  );
+}
--- react/src/router.jsx
+++ react/src/router.jsx
@@ -4,12 +4,19 @@ import Register from "./views/Register";
 import Login from "./views/Login";
 import Dashboard from "./views/Dashboard";
 import Surveys from "./views/Surveys";
+import GuestLayout from "./components/GuestLayout";
 
 const router = createBrowserRouter([
   { path: "/", element: <Dashboard /> },
   { path: "/surveys", element: <Surveys /> },
-  { path: "/login", element: <Login /> },
-  { path: "/register", element: <Register /> },
+  {
+    path: "/",
+    element: <GuestLayout />,
+    children: [
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+    ],
+  },
 ]);
 
 export default router;
