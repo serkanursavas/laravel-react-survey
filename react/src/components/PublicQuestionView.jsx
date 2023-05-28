73f54bc99cd222a703aed162d3cef68a17ac583a
serkanursavas <serkan.ursavas@icloud.com>
2023-05-28 16:14:25 +0300

    preparing public qeustion view

--- react/src/components/PublicQuestionView.jsx
+++ react/src/components/PublicQuestionView.jsx
@@ -1,5 +1,105 @@
 import React from 'react'
 
-export default function PublicQuestionView({ question, ind }) {
-  return <div>PublicQuestionView</div>
+export default function PublicQuestionView({ question, index, answerChanged }) {
+  let selectedOptions = []
+
+  function onCheckboxChange(option, $event) {
+    if ($event.target.checked) {
+      selectedOptions.push(option.uuid)
+    } else {
+      selectedOptions = selectedOptions.filter((op) => op != option.uuid)
+    }
+    answerChanged(selectedOptions)
+  }
+
+  return (
+    <>
+      <fieldset className="mb-4">
+        <div>
+          <legend className="text-base font-medium text-gray-900">
+            {index + 1}. {question.question}
+          </legend>
+          <p className="text-sm text-gray-500">{question.description}</p>
+        </div>
+
+        <div className="mt-3">
+          {question.type === 'select' && (
+            <div>
+              <select
+                onChange={(ev) => answerChanged(ev.target.value)}
+                className="block w-full px-3 py-2 mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              >
+                <option value="">Please Select</option>
+                {question.data.options.map((option) => (
+                  <option key={option.uuid} value={option.text}>
+                    {option.text}
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
+          {question.type === 'radio' && (
+            <div>
+              {question.data.options.map((option, ind) => (
+                <div key={option.uuid} className="flex items-center">
+                  <input
+                    value={option.text}
+                    onChange={(ev) => answerChanged(ev.target.value)}
+                    name={'question' + question.id}
+                    type="radio"
+                    id={option.uuid}
+                    className="w-4 h-4 text-indigo-600 border-gray-300 focus:ring-indigo-500"
+                  />
+                  <label
+                    htmlFor={option.uuid}
+                    className="block ml-3 text-sm font-medium text-gray-700"
+                  >
+                    {option.text}
+                  </label>
+                </div>
+              ))}
+            </div>
+          )}
+          {question.type === 'checkbox' && (
+            <div>
+              {question.data.options.map((option, ind) => (
+                <div key={option.uuid} className="flex items-center">
+                  <input
+                    id={option.uuid}
+                    onChange={(ev) => onCheckboxChange(option, ev)}
+                    type="checkbox"
+                    className="w-4 h-4 text-indigo-600 rounded focus:ring-indigo-500 border-r-gray-300"
+                  />
+                  <label
+                    htmlFor={option.uuid}
+                    className="block ml-3 text-sm font-medium text-gray-700"
+                  >
+                    {option.text}
+                  </label>
+                </div>
+              ))}
+            </div>
+          )}
+          {question.type === 'text' && (
+            <div>
+              <input
+                type="text"
+                onChange={(ev) => answerChanged(ev.target.value)}
+                className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+          )}
+          {question.type === 'textarea' && (
+            <div>
+              <textarea
+                onChange={(ev) => answerChanged(ev.target.value)}
+                className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 sm:text-sm"
+              ></textarea>
+            </div>
+          )}
+        </div>
+      </fieldset>
+      <hr className="mb-4" />
+    </>
+  )
 }
--- react/src/views/SurveyPublicView.jsx
+++ react/src/views/SurveyPublicView.jsx
@@ -4,23 +4,42 @@ import axiosClient from '../axios'
 import PublicQuestionView from '../components/PublicQuestionView'
 
 export default function SurveyPublicView() {
+  const answers = {}
   const { slug } = useParams()
-  const [survey, setSurvey] = useState({})
+  const [survey, setSurvey] = useState({
+    questions: [],
+  })
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     setLoading(true)
-    axiosClient.get(`survey/get-by-slug/${slug}`).then(({ data }) => {
-      setSurvey(data.data)
-      setLoading(false)
-    })
+    axiosClient
+      .get(`survey/get-by-slug/${slug}`)
+      .then(({ data }) => {
+        setSurvey(data.data)
+        setLoading(false)
+      })
+      .catch(() => {
+        setLoading(false)
+      })
   }, [])
+
+  function answerChanged(question, value) {
+    answers[question.id] = value
+    console.log(question, value)
+  }
+
+  function onSubmit(ev) {
+    ev.preventDefault()
+
+    console.log(answers)
+  }
 
   return (
     <div>
       {loading && <div className="flex justify-center">Loading</div>}
       {!loading && (
-        <form className="container mx-auto">
+        <form onSubmit={(ev) => onSubmit(ev)} className="container p-4 mx-auto">
           <div>
             <div className="grid grid-cols-6">
               <div className="mr-4">
@@ -40,12 +59,19 @@ export default function SurveyPublicView() {
               {survey.questions &&
                 survey.questions.map((question, index) => (
                   <PublicQuestionView
-                    key={index}
+                    key={question.id}
                     question={question}
                     index={index}
+                    answerChanged={(val) => answerChanged(question, val)}
                   />
                 ))}
             </div>
+            <button
+              type="submit"
+              className="inline-flex px-4 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm justify-centerpy-2 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Submit
+            </button>
           </div>
         </form>
       )}
